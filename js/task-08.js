65d379ad61485835c3463adaf3d993d24bb8ae64
PavlenkoDm <dmpavlenko07@gmail.com>
2023-01-30 22:50:18 +0300

    add task 8 solution

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const formEl = document.querySelector(".login-form");
+const inputEl = document.querySelector("[name='email']");
+const passwordEl = document.querySelector("[name='password']");
 
+formEl.addEventListener("submit", onSubmit);
+
+
+function onSubmit(event) {
+    event.preventDefault();
+
+    if (inputEl.value.length <= 0 || passwordEl.value.length <= 0) alert("Всі поля мають бути заповнені");
+
+    const incomeFormObj = {
+        [inputEl.getAttribute("name")]: inputEl.value,
+        [passwordEl.getAttribute("name")]: passwordEl.value
+    };
+
+    console.log(incomeFormObj);
+
+    event.currentTarget.reset();
+}
